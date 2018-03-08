9ce0036e5fc591eff13c747f5353ab46b986020b
patrickatcw <pj8392@yahoo.com>
2018-03-08 11:08:30 -0500

    adding objects to arraylist step 2

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //step 1
+        //step 1 declaring arraylist variable with capacity
         /*
         To create an array list in Java, you declare an ArrayList variable
         and call the ArrayList constructor to instantiate an ArrayList object
@@ -16,6 +16,18 @@ public class Main {
         You can optionally specific a capacity in the ArrayList constructor:
          */
         ArrayList<String> friends = new ArrayList<String>(5);
+
+        //step 2 adding objects to array list
+        /*
+        If you specified a type when you created the array list, the objects
+        you add via the add method must be of the correct type.
+         */
+        friends.add("Teena");
+        friends.add("Jim");
+        friends.add("Mitch");
+        friends.add("Bob");
+        friends.add("Sara");
+
 
     }
 
