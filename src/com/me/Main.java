f0c4fc8640e75d031ccde3d610b783910de68709
patrickatcw <pj8392@yahoo.com>
2018-03-08 11:02:54 -0500

    step 1 declaring an arraylist variable and declaring capacity

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,10 +1,22 @@
 package com.me;
+//http://www.dummies.com/programming/java/use-array-lists-in-java/
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-    }
+        //step 1
+        /*
+        To create an array list in Java, you declare an ArrayList variable
+        and call the ArrayList constructor to instantiate an ArrayList object
+        and assign it to the variable:
+        You can optionally specific a capacity in the ArrayList constructor:
+         */
+        ArrayList<String> friends = new ArrayList<String>(5);
 
+    }
 
 }
